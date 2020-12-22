5589f29bd1066e0a122c59fe246020b20361dd3c
Colton Medlin <coltonmedlin@gmail.com>
2020-12-21 22:44:36 -0800

    made all tests pass for arraySum

--- src/recursion.js
+++ src/recursion.js
@@ -30,6 +30,7 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  return array.reduce((accumulator, current) => (accumulator + (Array.isArray(current) ? arraySum(current) : current)), 0);
 };
 
 // 4. Check if a number is even.
